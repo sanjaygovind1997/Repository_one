ea600055c17a66cbded02d1f0e8bd91e36501e92
SANJAY GOVIND P <21016@fisat>
2018-04-18 01:13:00 -0400

    created fn to add 3 nos.

--- add.py
+++ add.py
@@ -1,4 +1,8 @@
 a=int(raw_input("Enter no.1"))
 b=int(raw_input("Enter no.2"))
 c=a+b
-print(c)
+print "sum of two nos.",c
+def add(num1,num2,num3):
+    s=num1+num2+num3
+    return s
+print "Sum of three:",add(2,3,4)
